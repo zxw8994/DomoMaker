78c7f39d922b40727b6c86e7ed641b17a7d3ba11
Zachary Wilken <zxw8994@rit.edu>
2017-04-06 21:54:40 -0400

    Adding more RAWR!. DomoMaker C

---
+++ server/router.js
@@ -0,0 +1,15 @@
+const controllers = require('./controllers');
+const mid = require('./middleware');
+
+const router = (app) => {
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+  app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
+  app.post('/maker', mid.requiresLogin, controllers.Domo.make);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+};
+
+module.exports = router;
